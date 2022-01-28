fd857abb0fef82fe34f6e9a8754663620a434f71
Selin Tuquet <selin.tuquet@gmail.com>
2022-01-28 11:39:12 -0500

    add functions to give plague and infect

--- cogs/plague.py
+++ cogs/plague.py
@@ -1,11 +1,40 @@
 from discord.ext import commands
-
+import discord
 
 class Plague(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    @commands.command()
+    async def give_plague(self,ctx: discord.Context, user: discord.Member):
+        role = discord.utils.get(lambda r: r.name == 'Plague', ctx.guild.roles)
+        channel = self.bot.get_channel(1234567890)
+        await user.add_roles(role)
+        old_nick = user.nick
+        await user.edit(nick = old_nick + "†")
+        await channel.send(f"Hey {user.name}, you now have {role.name}.")
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member: discord.Member, ctx: discord.Context, before: discord.VoiceState, after: discord.VoiceState):
+
+        role = discord.utils.get(lambda r: r.name == 'Plague', ctx.guild.roles)
+
+        if before.channel is None and after.channel is not None:
+
+            if role in member.roles:
+                return
+
+            channel = after.channel
+            members = channel.members
+
+
+            for other_member in members:
+                if role in other_member.roles:
+                    await self.give_plague(ctx, member)
+
+        else :
+            return
 
 
 def setup(bot: commands.Bot):
