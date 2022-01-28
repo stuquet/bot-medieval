6e71b35e79ffb4471edf4954f8aee3ebca436fc0
Snaptraks <snaptraks@gmail.com>
2022-01-28 09:54:23 -0500

    Reformat basiccommands.py

--- basiccommands.py
+++ basiccommands.py
@@ -1,4 +1,4 @@
-from discord.ext import commands # Again, we need this imported
+from discord.ext import commands  # Again, we need this imported
 
 
 class SomeCommands(commands.Cog):
@@ -10,7 +10,9 @@ class SomeCommands(commands.Cog):
     @commands.command(name="ping")
     async def ping(self, ctx: commands.Context):
         """Get the bot's current websocket latency."""
-        await ctx.send(f"Pong! {round(self.bot.latency * 1000)}ms") # It's now self.bot.latency
+        await ctx.send(
+            f"Pong! {round(self.bot.latency * 1000)}ms"
+        )  # It's now self.bot.latency
 
 
 def setup(bot: commands.Bot):
