a0f93a96b8f5cdf692885b507cc196ef239b81d8
Snaptraks <snaptraks@gmail.com>
2022-01-29 22:03:06 -0500

    Change give_plague command name to "infect"

--- cogs/plague.py
+++ cogs/plague.py
@@ -7,13 +7,14 @@ class Plague(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def give_plague(self,ctx: discord.Context, user: discord.Member):
+    async def infect(self,ctx: discord.Context, member: discord.Member):
+        """Infect a member with the role named Plague."""
         role = discord.utils.get(lambda r: r.name == 'Plague', ctx.guild.roles)
         channel = self.bot.get_channel(1234567890)
-        await user.add_roles(role)
-        old_nick = user.nick
-        await user.edit(nick = old_nick + "†")
-        await channel.send(f"Hey {user.name}, you now have {role.name}.")
+        await member.add_roles(role)
+        old_nick = member.nick
+        await member.edit(nick = old_nick + "†")
+        await channel.send(f"Hey {member.name}, you now have {role.name}.")
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member: discord.Member, ctx: discord.Context, before: discord.VoiceState, after: discord.VoiceState):
