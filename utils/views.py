753f393f655a120f123140d830dfe81a23932ffa
Snaptraks <snaptraks@gmail.com>
2022-02-15 16:03:37 -0500

    Add utils.views

---
+++ utils/views.py
@@ -0,0 +1,28 @@
+import discord
+
+
+# Define a simple View that gives us a confirmation menu
+# taken from https://github.com/Rapptz/discord.py/blob/master/examples/views/confirm.py
+class Confirm(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.value = None
+
+    # When the confirm button is pressed, set the inner value to `True` and
+    # stop the View from listening to more input.
+    # We also send the user an ephemeral message that we're confirming their choice.
+    @discord.ui.button(label="Confirm", style=discord.ButtonStyle.green)
+    async def confirm(
+        self, button: discord.ui.Button, interaction: discord.Interaction
+    ):
+        await interaction.response.send_message("Confirming", ephemeral=True)
+        self.value = True
+        self.stop()
+
+    # This one is similar to the confirmation button except sets the inner value
+    # to `False`
+    @discord.ui.button(label="Cancel", style=discord.ButtonStyle.red)
+    async def cancel(self, button: discord.ui.Button, interaction: discord.Interaction):
+        await interaction.response.send_message("Cancelling", ephemeral=True)
+        self.value = False
+        self.stop()
